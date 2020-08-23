9b0c3a104dfadff6376bd81d8d3de46f0324cd4a
FaresEl-Ansary <fareselansary1997@gmail.com>
2020-08-22 22:59:37 -0700

    Data Entry

---
+++ POSLoginScreen/DataEntry.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace POSLoginScreen
+{
+    partial class DataEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExpensesType = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPrices = new System.Windows.Forms.Button();
+            this.btnGamesCategories = new System.Windows.Forms.Button();
+            this.btnDevices = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnCategories = new System.Windows.Forms.Button();
+            this.btnItems = new System.Windows.Forms.Button();
+            this.btnMainCategories = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExpensesType
+            // 
+            this.btnExpensesType.BackColor = System.Drawing.Color.White;
+            this.btnExpensesType.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExpensesType.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExpensesType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnExpensesType.Location = new System.Drawing.Point(467, 214);
+            this.btnExpensesType.Name = "btnExpensesType";
+            this.btnExpensesType.Size = new System.Drawing.Size(146, 82);
+            this.btnExpensesType.TabIndex = 30;
+            this.btnExpensesType.Text = "Expenses Type";
+            this.btnExpensesType.UseVisualStyleBackColor = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBack.BackColor = System.Drawing.Color.White;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnBack.Location = new System.Drawing.Point(995, 11);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(74, 53);
+            this.btnBack.TabIndex = 29;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(121, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(308, 25);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "YOUR SUCCESS IS OUR SUCCESS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(558, 55);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Ain Shams University";
+            // 
+            // btnPrices
+            // 
+            this.btnPrices.BackColor = System.Drawing.Color.White;
+            this.btnPrices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrices.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrices.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnPrices.Location = new System.Drawing.Point(163, 170);
+            this.btnPrices.Name = "btnPrices";
+            this.btnPrices.Size = new System.Drawing.Size(146, 170);
+            this.btnPrices.TabIndex = 26;
+            this.btnPrices.Text = "Prices";
+            this.btnPrices.UseVisualStyleBackColor = false;
+            // 
+            // btnGamesCategories
+            // 
+            this.btnGamesCategories.BackColor = System.Drawing.Color.White;
+            this.btnGamesCategories.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGamesCategories.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGamesCategories.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnGamesCategories.Location = new System.Drawing.Point(315, 258);
+            this.btnGamesCategories.Name = "btnGamesCategories";
+            this.btnGamesCategories.Size = new System.Drawing.Size(146, 82);
+            this.btnGamesCategories.TabIndex = 25;
+            this.btnGamesCategories.Text = "Games Categories";
+            this.btnGamesCategories.UseVisualStyleBackColor = false;
+            // 
+            // btnDevices
+            // 
+            this.btnDevices.BackColor = System.Drawing.Color.White;
+            this.btnDevices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDevices.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDevices.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnDevices.Location = new System.Drawing.Point(315, 170);
+            this.btnDevices.Name = "btnDevices";
+            this.btnDevices.Size = new System.Drawing.Size(146, 82);
+            this.btnDevices.TabIndex = 24;
+            this.btnDevices.Text = "Devices (Rooms)";
+            this.btnDevices.UseVisualStyleBackColor = false;
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.BackColor = System.Drawing.Color.White;
+            this.btnEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployees.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployees.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnEmployees.Location = new System.Drawing.Point(619, 258);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(146, 82);
+            this.btnEmployees.TabIndex = 23;
+            this.btnEmployees.Text = "Employees";
+            this.btnEmployees.UseVisualStyleBackColor = false;
+            // 
+            // btnCategories
+            // 
+            this.btnCategories.BackColor = System.Drawing.Color.White;
+            this.btnCategories.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCategories.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategories.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnCategories.Location = new System.Drawing.Point(771, 258);
+            this.btnCategories.Name = "btnCategories";
+            this.btnCategories.Size = new System.Drawing.Size(146, 82);
+            this.btnCategories.TabIndex = 22;
+            this.btnCategories.Text = "Categories";
+            this.btnCategories.UseVisualStyleBackColor = false;
+            // 
+            // btnItems
+            // 
+            this.btnItems.BackColor = System.Drawing.Color.White;
+            this.btnItems.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnItems.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnItems.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnItems.Location = new System.Drawing.Point(619, 170);
+            this.btnItems.Name = "btnItems";
+            this.btnItems.Size = new System.Drawing.Size(146, 82);
+            this.btnItems.TabIndex = 21;
+            this.btnItems.Text = "Items";
+            this.btnItems.UseVisualStyleBackColor = false;
+            // 
+            // btnMainCategories
+            // 
+            this.btnMainCategories.BackColor = System.Drawing.Color.White;
+            this.btnMainCategories.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMainCategories.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainCategories.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(121)))), ((int)(((byte)(255)))));
+            this.btnMainCategories.Location = new System.Drawing.Point(771, 170);
+            this.btnMainCategories.Name = "btnMainCategories";
+            this.btnMainCategories.Size = new System.Drawing.Size(146, 82);
+            this.btnMainCategories.TabIndex = 20;
+            this.btnMainCategories.Text = "Main Categories";
+            this.btnMainCategories.UseVisualStyleBackColor = false;
+            // 
+            // DataEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.ClientSize = new System.Drawing.Size(1095, 530);
+            this.Controls.Add(this.btnExpensesType);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPrices);
+            this.Controls.Add(this.btnGamesCategories);
+            this.Controls.Add(this.btnDevices);
+            this.Controls.Add(this.btnEmployees);
+            this.Controls.Add(this.btnCategories);
+            this.Controls.Add(this.btnItems);
+            this.Controls.Add(this.btnMainCategories);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DataEntry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataEntry";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExpensesType;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPrices;
+        private System.Windows.Forms.Button btnGamesCategories;
+        private System.Windows.Forms.Button btnDevices;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnCategories;
+        private System.Windows.Forms.Button btnItems;
+        private System.Windows.Forms.Button btnMainCategories;
+    }
+}
